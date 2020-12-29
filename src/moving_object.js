d58418a23019df268e2226718bdd4867588f02d7
Jose Miralles <josemiralles1991@gmail.com>
2020-12-29 12:15:51 -0500

    Break time

---
+++ src/moving_object.js
@@ -0,0 +1,14 @@
+function MovingObject(options){
+
+}
+
+const mo = new MovingObject({
+    pos: [30, 30],
+    vel: [10, 10],
+    radius: 5,
+    color: "#00FF00"
+  });
+
+module.exports = {
+    MovingObject
+}
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+// Distance between two vectors:
+// Dist([x_1, y_1], [x_2, y_2]) = sqrt((x_1 - x_2) ** 2 + (y_1 - y_2) ** 2)
+
+// Norm of a vector:
+// Norm([x_1, y_1]) = Dist([0, 0], [x_1, y_1])
